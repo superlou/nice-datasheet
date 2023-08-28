d5866b571da1dee3797f10bde4e87d331d8e1411
superlou <lousimons@gmail.com>
2023-08-27 21:18:44 -0400

    Focusing sort of working

--- sheet.py
+++ sheet.py
@@ -5,19 +5,39 @@ from step import Step, ObservationStep
 class Sheet:
     def __init__(self):
         self.steps = []
+        self.current_step = 0
 
     def observe(self, id, text):
-        step = ObservationStep(id, text)
+        index = len(self.steps)
+        step = ObservationStep(
+            id, text,
+            self.advance,
+            lambda: self.got_focus(index)
+        )
         self.steps.append(step)
         return step
     
     def do(self, id, text):
-        step = Step(id, text)
+        index = len(self.steps)
+        step = Step(
+            id, text,
+            self.advance, 
+            lambda: self.got_focus(index)
+        )
         self.steps.append(step)
         return step
     
     def run(self):
+        self.current_step = 0
+
         for step in self.steps:
             step.to_ui()
     
         ui.run()
+    
+    async def advance(self):
+        self.current_step += 1
+        await self.steps[self.current_step].focus()
+
+    def got_focus(self, index):
+        self.current_step = index
--- step.py
+++ step.py
@@ -2,9 +2,11 @@ from nicegui import ui
 
 
 class Step:
-    def __init__(self, id, text):
+    def __init__(self, id, text, advance_fn=None, got_focus_fn=None):
         self.id = id
         self.text = text
+        self.advance_fn = advance_fn
+        self.got_focus_fn = got_focus_fn
 
     def to_ui(self):
         with ui.row().classes("items-center w-full max-w-screen-md"):
@@ -23,6 +25,9 @@ class Step:
 
         self.compliance_prev = self.compliance.value
 
+    async def focus(self):
+        await ui.run_javascript(f"getElement({self.compliance.id}).$el.firstChild.focus()")
+
     def update_compliance_color(self, event=None):
         if self.compliance.value == "Pass":
             self.compliance.classes(add="toggle-pass", remove="toggle-fail")
@@ -36,11 +41,14 @@ class Step:
 
 
 class ObservationStep(Step):
-    def __init__(self, id, text):
+    def __init__(self, id, text, advance_fn=None, got_focus_fn=None):
         self.id = id
         self.text = text
         self.observe_fn = None
         self.spec = None
+        self.validate_fn = None
+        self.advance_fn = advance_fn
+        self.got_focus_fn = got_focus_fn
 
     def capture(self, observe_fn):
         self.observe_fn = observe_fn
@@ -71,19 +79,27 @@ class ObservationStep(Step):
             lambda: self.reset_toggle_if_click_same(self.compliance.value)
         )
 
+        self.input.on("focusin", self.got_focus_fn)
+
         self.input.on("keypress", self.validate)
         self.compliance.on("update:model-value", self.update_compliance_color)
 
     def observe(self):
         measurement = self.observe_fn()
         self.input.set_value(measurement)
+        self.input.run_method("focus")
 
-    def validate(self, event):
+    def focus(self):
+        self.input.run_method("focus")
+
+    async def validate(self, event):
         if event.args["keyCode"] != 13:
             return
 
         if self.validate_fn is None:
+            await self.advance_fn()
             return
         
         self.compliance.set_value("Pass" if self.validate_fn(self.input.value) else "Fail")
         self.update_compliance_color()
+        await self.advance_fn()
